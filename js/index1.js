d1470d46b039e368f3b584a2d302dd212b630550
jianhua12 <1161338679@qq.com>
2017-12-26 21:13:52 +0800

    a
    Signed-off-by: jianhua12 <1161338679@qq.com>

--- js/index1.js
+++ js/index1.js
@@ -1,26 +1,18 @@
  
 
-  $(function(){
-     $(".banniu").click(function(){
-    $(".ul33").toggle();
-   });
-  });
-
-  $(function(){
-    $('.c1_m').click(function(){
-      $('.ul33').hide();
+    $(function(){
+      $('.navbutton').click(function(){
+        $('.nvatop').toggle();
+      })
+   
     })
   
-  })
 
-  $(function(){
-      $('.c1_m').click(function(){
-      $('.index-top-ul').hide();
-    })
-    $('.danniu').click(function(){
-      $('.index-top-ul').show();
-    })
-    
-  })
-
-  
+$(function(){
+  $('.c1_m').click( function(){
+    if(window.innerWidth < 769){
+        $('.nvatop').hide();
+        $('.navli').css('background-color','none')
+      }
+     })
+})
